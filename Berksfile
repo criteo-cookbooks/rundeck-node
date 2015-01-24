2a801d9df6e2b08e85b06e268d310a9f9272574c
Baptiste Courtois <b.courtois@criteo.com>
2015-01-24 11:26:00 +0100

    Depends on openssl & winrm-config

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.getchef.com'
 
 metadata
+
+cookbook 'winrm-config', git: 'https://github.com/criteo-cookbooks/winrm-config.git'
--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends          'sudo'
+depends          'openssl'
+depends          'winrm-config'
 
 suggests         'rundeck-server'
 suggests         'rundeck-bridge'
